38806741074a9c1e7657a13a0a0b9d8cf75ef968
Eugen <lipen77777venk@mail.ru>
2022-09-13 18:12:30 +0400

    f-string

---
+++ f_string.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  age := 30
+  name := "Jon"
+  value := 100.679
+  bul := true
+  fmt.Printf("My age is %d! My name is %s. I have %f dollars. %t", age, name, value, bul)
+}
