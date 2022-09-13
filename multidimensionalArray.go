b1d5b5668585b48197772316cce8616baa89d992
Eugen <lipen77777venk@mail.ru>
2022-09-13 12:52:55 +0400

    array 3+

---
+++ multidimensionalArray.go
@@ -0,0 +1,20 @@
+package main
+
+import(
+  "fmt"
+)
+
+func main(){
+  array := [3] [4]int{{1,2,3,4},
+                      {5,6,7,8},
+                      {9,10,11,12}}
+  fmt.Println(array[1])
+  fmt.Println(array[1][2])
+
+  for i := 0; i < len(array); i++ {
+    fmt.Println(array[i])
+    for j := 0; j < len(array[i]);j++ {
+      fmt.Println(array[i][j])
+    }
+  }
+}
